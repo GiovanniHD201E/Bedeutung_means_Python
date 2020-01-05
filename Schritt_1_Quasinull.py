8f78d61d8455510facb2b3dbda4c83be0d867b05
GiovanniHD201E <42226804+GiovanniHD201E@users.noreply.github.com>
2020-01-05 17:43:47 -0300

    Create Schritt_1_Quasinull.py

---
+++ Schritt_1_Quasinull.py
@@ -0,0 +1,2 @@
+#Wenn zwei Elemente sind näher, welche Wechseln tritten zwischenbeiden ein?
+Klass = ['Umgebung', 'C-H-O-N-P-S'] # Environment and 'Pseudo Chemical Elements, Carbon, Hydrogeny, etc..
