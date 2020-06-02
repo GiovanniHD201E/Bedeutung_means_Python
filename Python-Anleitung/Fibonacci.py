2d9eedcc2e3db4f5d0757fcfddd22f1dcc583a6e
Giovanni_Medeiros <giovanni_medeiros@outlook.com>
2020-06-02 10:49:34 -0300

    A descartar!

--- Python-Anleitung/Fibonacci.py
+++ Python-Anleitung/Fibonacci.py
@@ -1,7 +1,25 @@
-#Fibonacci-Folge
-#Die Summe von letzte zwei Elemente ist die aktuelle Element.
-a, b = 0, 1
-while b < 10:
-  print (b)
-  a, b = b, a + b
-  
+# Fibonacci-Folge und Hyperaktivitätsstorung
+# Die Summe von letzte zwei Elemente ist die aktuelle Element.
+  # Meinerseits²
+def fibo(n):
+    a, b = 0, 1
+      while b <= n:
+            print (b)
+            a, b = b, a + b
+    while b <= n:
+          a, b = b, a + b
+    return b
+
+def SH(smart = 10, Hyperaktiv = 01)
+    'Is there any pythonic or C Methode um length in cm?'
+    'Well, für nicht festlegt Breiesschriftart, die Maß von\n\
+      String durch len Funktion ist nicht präsize, obwohl \n\
+        immer noch besser als mein deutsche Text!'
+    x = fibo(smart) + len('def fibo(n)')
+    return x
+
+class GioFormaTrump:
+
+    def __init__(self):
+        self = 'whorules'
+        
