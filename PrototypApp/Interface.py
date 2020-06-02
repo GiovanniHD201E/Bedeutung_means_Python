47126c43a1bf8b0a31460deea12e2727ba667890
Giovanni_Medeiros <giovanni_medeiros@outlook.com>
2020-06-02 11:21:14 -0300

    Margin scratch

--- PrototypApp/Interface.py
+++ PrototypApp/Interface.py
@@ -1,4 +1,8 @@
-"""import Angela1
-import algefamilie
-"""
-import algefamilie 
+def top_Bottom(width = 10, alignment = "left"):
+    x = '*' * width
+    if alignment != "left":
+        # wait...
+        pass
+    else:
+        return x
+    print(x)
