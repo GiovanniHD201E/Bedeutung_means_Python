d42ad5bc7b27cef6708e33883e3f5ed0463a3287
Giovanni_Medeiros <giovanni_medeiros@outlook.com>
2020-06-03 10:45:41 -0300

    Fixing inconsistences indicated by VSC

--- PrototypApp/Interface.py
+++ PrototypApp/Interface.py
@@ -1,8 +1,20 @@
-def top_Bottom(width = 10, alignment = "left"):
+def stars(width=10):
     x = '*' * width
-    if alignment != "left":
-        # wait...
-        pass
-    else:
-        return x
     print(x)
+
+def m(text = "queimei a mão no fogão"):
+    print('*' * 10)
+    print(text)
+
+texto = 'Queimei a mäo nao fogo'
+    
+"""padronizando a impressão de saída com a função print"""
+
+
+tip = [
+    'Para ver o que este programa faz, você precisa chamar uma das rotinas definidas no programa:',
+    'Este programa não está pronto,',
+    'por enquanto as duas executam a mesma tarefa',
+    'm() ou stars(). ESCREVA:   \n   m() e tecle ENTER']
+
+print(tip[3].split('.')[1])
